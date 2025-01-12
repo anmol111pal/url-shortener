2a7e19d23f8ea64c6d8e7bb12cd64f4662247141
Anmol Pal <anmol111pal@gmail.com>
2025-01-12 23:44:38 +0530

    Configured the mongo URI for a new cluster

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const URI='mongodb+srv://anmol:PPap1310@url-shortener.suefq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const URI='mongodb+srv://anmol111pal:PPap1310@cluster0-url-shortener.g8gia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-URL-Shortener'
 
 const connectDB=async ()=> {
     try {
